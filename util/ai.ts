d8fa9e33e2cd9e1b92dc9549fef86b1148e5651b
yacine_datascience <135754800+yacinedatadev@users.noreply.github.com>
2023-12-23 17:29:54 +0100

    final

--- util/ai.ts
+++ util/ai.ts
@@ -145,3 +145,93 @@ export const calculate_score_A = async (entry: { landmark: Point }) => {
     const score_a = await Rula_table_A[rowIndex][columnIndex];
     return score_a;
 }
+
+function calculate_neck_score(landmarks: Landmark[]) {
+    let neck_score = 0;
+    const nose_neck_trunk_angle = /* calculate angle */;
+    const neck_trunk_feet_angle = /* calculate angle */;
+
+    if (0 < nose_neck_trunk_angle && nose_neck_trunk_angle < 10) {
+        neck_score = neck_score + 1;
+    }
+    if (10 < neck_trunk_feet_angle && neck_trunk_feet_angle < 20) {
+        neck_score = neck_score + 2;
+    }
+    if (neck_trunk_feet_angle > 20) {
+        neck_score = neck_score + 3;
+    }
+    if (neck_trunk_feet_angle < 0) {
+        neck_score = neck_score + 4;
+    }
+    // if neck is twisted +1
+    if (landmarks[11].x != landmarks[12].x) {
+        neck_score = neck_score + 1;
+    }
+    // if neck is bending +1
+    if (landmarks[11].y != landmarks[12].y) {
+        neck_score = neck_score + 1;
+    }
+    return neck_score;
+}
+
+function calculate_trunk_score(landmarks: Landmark[]) {
+    let trunk_score = 0;
+    const neck_trunk_feet_angle = /* calculate angle */;
+
+    if (neck_trunk_feet_angle == 0) {
+        trunk_score = trunk_score + 1;
+    }
+    if (0 < neck_trunk_feet_angle && neck_trunk_feet_angle < 20) {
+        trunk_score = trunk_score + 2;
+    }
+    if (20 < neck_trunk_feet_angle && neck_trunk_feet_angle < 60) {
+        trunk_score = trunk_score + 3;
+    }
+    if (neck_trunk_feet_angle > 60) {
+        trunk_score = trunk_score + 4;
+    }
+    // if trunk is twisted +1
+    if (landmarks[23].x != landmarks[24].x) {
+        trunk_score = trunk_score + 1;
+    }
+    // if trunk is bending +1
+    if (landmarks[23].y != landmarks[24].y) {
+        trunk_score = trunk_score + 1;
+    }
+    return trunk_score;
+}
+
+function calculate_leg_score(landmarks: Landmark[]) {
+    let leg_score = 0;
+    if (landmarks[27].x != landmarks[28].x) {
+        leg_score = leg_score + 1;
+    }
+    if (landmarks[27].y != landmarks[28].y) {
+        leg_score = leg_score + 1;
+    }
+    return leg_score;
+}
+
+function get_rula_score_B(trunk_score: number, leg_score: number, neck_score: number) {
+    const columnIndex = (trunk_score - 1) * 2 + (leg_score - 1);
+    return Rula_table_B[neck_score][columnIndex];
+}
+
+function get_rula_score(posture_score_A: number, posture_score_B: number, muscle_use_score: number, force_load_score: number) {
+    const wrist_arm_score = posture_score_A + muscle_use_score + force_load_score;
+    const neck_trunk_leg_score = posture_score_B + muscle_use_score + force_load_score;
+    return Rula_table_C[wrist_arm_score][neck_trunk_leg_score];
+}
+
+const wrist_score = calculate_wrist_score(landmarks);
+const wrist_twist_score = calculate_wrist_twist_score(landmarks);
+const upper_arm_score = calculate_upper_arm_score(right_shoulder_angle, left_shoulder_angle);
+const lower_arm_score = calculate_lower_arm_score(landmarks);
+const neck_score = calculate_neck_score(landmarks);
+const trunk_score = calculate_trunk_score(landmarks);
+const leg_score = calculate_leg_score(landmarks);
+const Rula_Table_a_score = get_rula_score_A(wrist_score, wrist_twist_score, upper_arm_score, lower_arm_score);
+const Rula_table_B_score = get_rula_score_B(trunk_score, leg_score, neck_score);
+const Rula_score = get_rula_score(posture_score_A, posture_score_B, muscle_use_score, force_load_score);
+
+console.log(Rula_score);
